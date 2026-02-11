d6fdf0c751ac990edda6e7e82fb5b501130417ea
EliteHaze <chmcs2026.limesh.chaudhari@gmail.com>
2026-02-11 10:25:32 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 
 import axios from "axios"
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    //baseURL: 'http://localhost:3000'
+    baseURL:'https://mern-bookstore-8lcy.onrender.com'
 })
 export default api
